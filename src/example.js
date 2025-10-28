957b378b69216db7dae7cd439a47572b00e8306e
Kedaya <111995151+ZJF-Kedaya@users.noreply.github.com>
2025-10-28 23:15:16 +0800

    Update example.js

--- src/example.js
+++ src/example.js
@@ -1,7 +1,7 @@
 export default {
     "tencent": {
         "playlist": { "show": true, "value": "7326220405" },
-        "song": { "show": true, "value": "002Rnpvi058Qdm" },
+        "song": { "show": true, "value": "003uh6Y81FI4Nv" },
         "lrc": { "show": false, "value": "000i26Sh1ZyiNU" },
         "pic": { "show": false, "value": "002Rnpvi058Qdm" },
         "url": { "show": false, "value": "002Rnpvi058Qdm" },
