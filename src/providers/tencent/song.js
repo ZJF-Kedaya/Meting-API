205818016c66a0451bcf7fb220d3749cf8a058a7
Kedaya <111995151+ZJF-Kedaya@users.noreply.github.com>
2025-10-29 10:29:56 +0800

    Update song.js

--- src/providers/tencent/song.js
+++ src/providers/tencent/song.js
@@ -1,11 +1,31 @@
 import { changeUrlQuery } from "./util.js"
 import config from "../../config.js"
 
-export const get_song_url = async (id, cookie = '') => {
+// 带代理和cookie的请求工具函数
+const fetchWithProxy = async (url, cookie = '') => {
+    // 若配置了代理，使用代理地址拼接请求URL
+    const fetchUrl = config.QQ_MUSIC_PROXY ? `${config.QQ_MUSIC_PROXY}${url}` : url;
+    const headers = {
+        'Cookie': cookie,
+        'Referer': 'https://y.qq.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+    };
+    return fetch(fetchUrl, { headers });
+}
 
+export const get_song_url = async (id, cookie = '') => {
     id = id.split(',')
     let uin = ''
     let qqmusic_key = ''
+    
+    // 从cookie中提取uin和qqmusic_key（会员验证关键参数）
+    if (cookie) {
+        const uinMatch = cookie.match(/uin=(\d+)/);
+        if (uinMatch) uin = uinMatch[1];
+        const keyMatch = cookie.match(/qqmusic_key=([^;]+)/);
+        if (keyMatch) qqmusic_key = keyMatch[1];
+    }
+
     const typeObj = {
         s: 'M500',
         e: '.mp3',
@@ -21,12 +41,12 @@ export const get_song_url = async (id, cookie = '') => {
             module: 'vkey.GetVkeyServer',
             method: 'CgiGetVkey',
             param: {
-                // filename: file,
+                filename: file,  // 恢复filename参数（关键）
                 guid: guid,
                 songmid: id,
                 songtype: [0],
-                uin: uin,
-                loginflag: 1,
+                uin: uin,        // 使用cookie中的uin
+                loginflag: cookie ? 1 : 0,  // 登录状态标记
                 platform: '20',
             },
         },
@@ -35,14 +55,14 @@ export const get_song_url = async (id, cookie = '') => {
             format: 'json',
             ct: 19,
             cv: 0,
-            authst: qqmusic_key,
+            authst: qqmusic_key,  // 使用cookie中的验证key
         },
     }
 
     let params = {
         '-': 'getplaysongvkey',
         g_tk: 5381,
-        loginUin: uin,
+        loginUin: uin,  // 传递uin
         hostUin: 0,
         format: 'json',
         inCharset: 'utf8',
@@ -51,7 +71,6 @@ export const get_song_url = async (id, cookie = '') => {
         needNewCode: 0,
         data: JSON.stringify(data),
     }
-
 
     if (config.OVERSEAS || id.length > 1) {
         params.format = 'jsonp'
@@ -62,13 +81,10 @@ export const get_song_url = async (id, cookie = '') => {
         return "@" + parse_function + '@' + callback_name + '@' + callback_function_name + '@' + url
     }
 
-
     const url = changeUrlQuery(params, 'https://u.y.qq.com/cgi-bin/musicu.fcg')
-
-    let result = await fetch(url);
-
+    let result = await fetchWithProxy(url, cookie);  // 使用代理请求并传递cookie
     result = await result.json()
-    // console.log(result)
+
     if (result.req_0 && result.req_0.data && result.req_0.data.midurlinfo) {
         purl = result.req_0.data.midurlinfo[0].purl;
     }
@@ -78,9 +94,7 @@ export const get_song_url = async (id, cookie = '') => {
         result.req_0.data.sip[0];
 
     const res = `${domain}${purl}`.replace('http://', 'https://')
-    // console.log(res);
     return res;
-
 }
 
 export const get_song_info = async (id, cookie = '') => {
@@ -97,35 +111,22 @@ export const get_song_info = async (id, cookie = '') => {
     };
 
     const url = changeUrlQuery(data, 'http://u.y.qq.com/cgi-bin/musicu.fcg');
-
-    let result = await fetch(url);
-
+    let result = await fetchWithProxy(url, cookie);  // 使用代理请求并传递cookie
     result = await result.json()
-
     result = result.songinfo.data
 
     let song_info = {
         author: result.track_info.singer.reduce((i, v) => ((i ? i + " / " : i) + v.name), ''),
         title: result.track_info.name,
         pic: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${result.track_info.album.mid}.jpg`,
-        url: config.OVERSEAS ? await get_song_url(id) : id,
+        url: config.OVERSEAS ? await get_song_url(id, cookie) : id,  // 传递cookie
         lrc: id,
         songmid: id,
     }
-    // console.log(song_info)
     return [song_info]
 }
 
 export const get_pic = async (id, cookie = '') => {
-    const info = await get_song_info(id, cookie)
+    const info = await get_song_info(id, cookie)  // 传递cookie
     return info[0].pic
 }
-
-// const res = await get_song_url('002Rnpvi058Qdm');
-// console.log(res)
-
-// const res = await get_song_url('002Rnpvi058Qdm,000i26Sh1ZyiNU');
-// console.log(res)
-
-// const res = await get_song_info('002Rnpvi058Qdm');
-// console.log(res)
